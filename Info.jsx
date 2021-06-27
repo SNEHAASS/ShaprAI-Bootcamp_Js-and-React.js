85b0e2ee8b89c84c3a7837343d9e2b328d7d4244
SNEHAASS <86572706+SNEHAASS@users.noreply.github.com>
2021-06-28 02:59:02 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken by Mr.Shaurya. We covered everything
+        from scratch including java script, React.js,HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
